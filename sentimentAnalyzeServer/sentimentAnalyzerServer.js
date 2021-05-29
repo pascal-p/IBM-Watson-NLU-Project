f74ee56102cd8e3ccb358cdede561a5dccdc44a7
Pascal P <lacsap_666@yahoo.fr>
2021-05-29 15:33:00 +1200

    iter 1

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,5 +1,26 @@
 const express = require('express');
 const app = new express();
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const getNLUInstance = () => {
+  let api_key = process.env.API_KEY;
+  let api_url = process.env.API_URL;
+
+  const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+  const { IamAuthenticator } = require('ibm-watson/auth');
+
+  const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+    version: '2020-08-01',
+    authenticator: new IamAuthenticator({
+      apikey: '{apikey}',
+    }),
+    serviceUrl: '{url}',
+  });
+
+  return naturalLanguageUnderstanding;
+}
 
 app.use(express.static('client'))
 
@@ -30,4 +51,3 @@ app.get("/text/sentiment", (req,res) => {
 let server = app.listen(8080, () => {
     console.log('Listening', server.address().port)
 })
-
