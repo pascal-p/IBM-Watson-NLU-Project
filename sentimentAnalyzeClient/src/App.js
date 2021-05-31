d1eaf97dffa36e70be34d7a52e0b40cc0f128fef
Pascal P <lacsap_666@yahoo.fr>
2021-05-31 20:34:13 +1200

    client side: min. completion

--- sentimentAnalyzeClient/src/App.js
+++ sentimentAnalyzeClient/src/App.js
@@ -5,31 +5,32 @@ import React from 'react';
 import axios from 'axios';
 
 class App extends React.Component {
-  state = {innercomp:<textarea rows="4" cols="50" id="textinput"/>,
-            mode: "text",
-          sentimentOutput:[],
-          sentiment:true
-        }
-  
-  renderTextArea = ()=>{
-    document.getElementById("textinput").value = "";
-    if(this.state.mode === "url") {
-      this.setState({innercomp:<textarea rows="4" cols="50" id="textinput"/>,
-      mode: "text",
-      sentimentOutput:[],
-      sentiment:true
-    })
-    } 
+  state = {
+    innercomp:<textarea rows="4" cols="50" id="textinput"/>,
+    mode: "text",
+    sentimentOutput:[],
+    sentiment:true
   }
 
-  renderTextBox = ()=>{
+  renderTextArea = () => {
+    document.getElementById("textinput").value = "";
+    if(this.state.mode === "url") {
+      this.setState({innercomp:<textarea rows="2" cols="90" id="textinput"/>,
+                     mode: "text",
+                     sentimentOutput:[],
+                     sentiment:true
+                    })
+    }
+  }
+
+  renderTextBox = () => {
     document.getElementById("textinput").value = "";
     if(this.state.mode === "text") {
       this.setState({innercomp:<textarea rows="1" cols="50" id="textinput"/>,
-      mode: "url",
-      sentimentOutput:[],
-      sentiment:true
-    })
+                     mode: "url",
+                     sentimentOutput:[],
+                     sentiment:true
+                    })
     }
   }
 
@@ -38,26 +39,33 @@ class App extends React.Component {
     let ret = "";
     let url = ".";
 
-    if(this.state.mode === "url") {
-      url = url+"/url/sentiment?url="+document.getElementById("textinput").value;
-    } else {
-      url = url+"/text/sentiment?text="+document.getElementById("textinput").value;
+    if (this.state.mode === "url") {
+      url = url + "/url/sentiment?url=" + document.getElementById("textinput").value;
     }
+    else {
+      url = url + "/text/sentiment?text=" + document.getElementById("textinput").value;
+    }
+
     ret = axios.get(url);
-    ret.then((response)=>{
-
-      //Include code here to check the sentiment and fomrat the data accordingly
-
-      this.setState({sentimentOutput:response.data});
-      let output = response.data;
-      if(response.data === "positive") {
-        output = <div style={{color:"green",fontSize:20}}>{response.data}</div>
-      } else if (response.data === "negative"){
-        output = <div style={{color:"red",fontSize:20}}>{response.data}</div>
-      } else {
-        output = <div style={{color:"orange",fontSize:20}}>{response.data}</div>
+    ret.then((resp) => {
+      // Include code here to check the sentiment and format the data accordingly
+      let output = "";
+      if(resp.data === "positive") {
+        output = <div style={{color:"darkgreen", fontSize:18}}>{resp.data}</div>
       }
-      this.setState({sentimentOutput:output});
+      else if (resp.data === "negative") {
+        output = <div style={{color:"darkred", fontSize:18}}>{resp.data}</div>
+      }
+      else {
+        // neutral
+        output = <div style={{color:"lightyellow", background:"black", fontSize:18}}>{resp.data}</div>
+      }
+
+      this.setState({sentimentOutput: output});
+    }).catch(error => {
+      console.log("error in resp: ", error)
+      error = <div>{{error}}</div>;
+      this.setState({sentimentOutput: error});
     });
   }
 
@@ -65,34 +73,41 @@ class App extends React.Component {
     this.setState({sentiment:false});
     let ret = "";
     let url = ".";
-    if(this.state.mode === "url") {
-      url = url+"/url/emotion?url="+document.getElementById("textinput").value;
-    } else {
-      url = url+"/text/emotion/?text="+document.getElementById("textinput").value;
+
+    if (this.state.mode === "url") {
+      url = url + "/url/emotion?url=" + document.getElementById("textinput").value;
     }
+    else {
+      url = url + "/text/emotion/?text=" + document.getElementById("textinput").value;
+    }
+
     ret = axios.get(url);
 
-    ret.then((response)=>{
-      this.setState({sentimentOutput:<EmotionTable emotions={response.data}/>});
-  });
+    ret.then((resp) => {
+      this.setState({sentimentOutput:<EmotionTable emotions={resp.data}/>});
+    }).catch(error => {
+      console.log("error in resp: ", error)
+      error = <div>{{error}}</div>;
+      this.setState({sentimentOutput: error});
+    });
   }
-  
+
 
   render() {
-    return (  
+    return (
       <div className="App">
-      <button className="btn btn-info" onClick={this.renderTextArea}>Text</button>
-        <button className="btn btn-dark"  onClick={this.renderTextBox}>URL</button>
-        <br/><br/>
+        <button className="btn btn-info" onClick={this.renderTextArea}>Text</button>
+        <button className="btn btn-dark" onClick={this.renderTextBox}>URL</button>
+        <br /><br />
         {this.state.innercomp}
-        <br/>
+        <br />
         <button className="btn-primary" onClick={this.sendForSentimentAnalysis}>Analyze Sentiment</button>
         <button className="btn-primary" onClick={this.sendForEmotionAnalysis}>Analyze Emotion</button>
-        <br/>
-            {this.state.sentimentOutput}
+        <br />
+        {this.state.sentimentOutput}
       </div>
     );
-    }
+  }
 }
 
 export default App;
--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -3,20 +3,25 @@ import './bootstrap.min.css';
 
 class EmotionTable extends React.Component {
     render() {
-      return (  
-        <div>
-          {/*You can remove this line and the line below. */}
-          {JSON.stringify(this.props.emotions)}
+      return (
+        <div className="mt-1 ml-1 mr-1">
           <table className="table table-bordered">
             <tbody>
             {
-                //Write code to use the .map method that you worked on in the Hands-on React lab to extract the emotions
+              // Write code to use the .map method that you worked on in the Hands-on React lab to extract the emotions
+              Object.entries(this.props.emotions).map((emo) => {
+                return (
+                  <tr>
+                    <td>{emo[0]}</td><td>{emo[1]}</td>
+                  </tr>
+                );
+              })
             }
             </tbody>
           </table>
-          </div>
-          );
-        }
-    
+        </div>
+      );
+    }
+
 }
 export default EmotionTable;
